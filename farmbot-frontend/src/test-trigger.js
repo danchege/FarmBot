1894cdedbdc0f299797041be7d06b8bc52f62ad6
danchege <danychege28@gmail.com>
2025-08-04 10:35:40 +0300

    test: trigger workflow with working directory fix

---
+++ farmbot-frontend/src/test-trigger.js
@@ -0,0 +1,4 @@
+// Test file to trigger GitHub Actions workflow
+// This file can be deleted after the workflow runs successfully
+
+console.log('Triggering workflow test - ' + new Date().toISOString()); 
